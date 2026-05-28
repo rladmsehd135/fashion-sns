0ba50a60260e338de0344efae81a4d198b37914f
rladmsehd135 <rladmsehd135@gmail.com>
2026-05-28 12:06:27 +0900

    Delete server directory

--- server/config/jwt.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  accessSecret:  process.env.JWT_ACCESS_SECRET,
-  refreshSecret: process.env.JWT_REFRESH_SECRET,
-  accessExpire:  process.env.JWT_ACCESS_EXPIRE,
-  refreshExpire: process.env.JWT_REFRESH_EXPIRE,
-};

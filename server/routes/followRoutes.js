d0057855cf46205439ec75987bdd04f87a618643
eundong <rladmsehd135@gmail.com>
2026-05-28 12:09:31 +0900

    2026 -05- 28

--- server/routes/followRoutes.js
+++ server/routes/followRoutes.js
@@ -2,6 +2,7 @@ const router          = require('express').Router();
 const FollowController = require('../controllers/followController');
 const authMiddleware  = require('../middlewares/authMiddleware');
 
+
 router.post('/:userId', authMiddleware, FollowController.toggle);
 
 module.exports = router;
